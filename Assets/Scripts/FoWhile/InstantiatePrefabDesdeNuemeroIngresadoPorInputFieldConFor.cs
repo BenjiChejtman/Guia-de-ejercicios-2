6623c23b6733382630c1e09ed35648e2731023f4
BenjiChejtman <46895375@est.ort.edu.ar>
2022-05-21 18:20:12 -0300

    Excs for and while completed

---
+++ Assets/Scripts/FoWhile/InstantiatePrefabDesdeNuemeroIngresadoPorInputFieldConFor.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstantiatePrefabDesdeNuemeroIngresadoPorInputFieldConFor : MonoBehaviour
+{
+    public GameObject prefab;
+    public Text InputCantidad;
+    int cant;
+
+    public void InstantiatePrefabCantidadDeVecesDesdeInputFieldConFor()
+    {
+        cant = int.Parse(InputCantidad.text);
+        
+        if (cant >= 0)
+        {
+            for (int i = 0; i < cant; i++)
+            {
+                Instantiate(prefab);
+            }
+        }
+        else if (cant < 0)
+        {
+            Debug.Log("Porfavor, elija un numero que no sea negativo.");
+        }
+        
+        
+    }
+}
