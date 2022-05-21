4e0d46c224da3748878b4a2cf61d7cce1de5be94
BenjiChejtman <46895375@est.ort.edu.ar>
2022-05-21 20:16:28 -0300

    Almsot finished Input and continued with prefabs and instantiate

---
+++ Assets/Scripts/Input/AvanzaRetrocedeGiraDerechaEIzquierda.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AvanzaRetrocedeGiraDerechaEIzquierda : MonoBehaviour
+{
+    public float movement;
+    public float movement2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position += new Vector3(0, 0, movement);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position -= new Vector3(0, 0, movement);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.eulerAngles += new Vector3(0, movement2, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.eulerAngles -= new Vector3(0, movement2, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Prefab and Instantiate/Ejercicio 3/InstantiateOnlyOnce.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantiateOnlyOnce : MonoBehaviour
+{
+    public GameObject cube;
+    int counter = 0;
+
+    public void InstantiateCubeOnlyOnce()
+    {
+        while (counter < 1)
+        {
+            Instantiate(cube);
+            counter++;
+        }
+            Debug.Log("Intancia ya creada.");
+    }
+}
