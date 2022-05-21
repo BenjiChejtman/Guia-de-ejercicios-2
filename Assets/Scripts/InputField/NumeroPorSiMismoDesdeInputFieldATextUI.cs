dcec8624c1d38d1bdd409801dda985f88c49cdfb
BenjiChejtman <46895375@est.ort.edu.ar>
2022-05-21 16:58:04 -0300

    Excs 1,2 completed, 3, 4 incomplete Inputfield

---
+++ Assets/Scripts/InputField/NumeroPorSiMismoDesdeInputFieldATextUI.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NumeroPorSiMismoDesdeInputFieldATextUI : MonoBehaviour
+{
+    public Text NumeroDesdeInputField;
+    public Text TextUI;
+    int num;
+    int res;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void NumeroPorSiMismo()
+    {/*
+        if (NumeroDesdeInputField.text != "")
+        {
+            num = int.Parse(NumeroDesdeInputField.text);
+            res = num * num;
+            TextUI.text = res;
+        }
+        else if (NumeroDesdeInputField.text == "")
+        {
+            Debug.Log("Error, ingrese algo en el inputfield.");
+        }*/
+    }
+    
+}
---
+++ Assets/Scripts/InputField/TextenUIdeInputfield.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextenUIdeInputfield : MonoBehaviour
+{
+    public Text inputfield;
+    public Text TextUI;
+
+    public void InputfieldTextInTextUI()
+    {
+        if(inputfield.text != "")
+        {
+            TextUI.text = inputfield.text;
+        }
+        else if (inputfield.text == "")
+        {
+            Debug.Log("Error, ingrese algo en el inputfield.");
+        }
+    }
+}
